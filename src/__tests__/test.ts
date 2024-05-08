bcf40c7ade25c3b9467a978b958a3e21fc920839
Boris Gbahoue <bgbahoue@gmail.com>
2024-05-08 11:58:45 +1100

    Add: simple test implementation
    * Will be used for now until we move to something fancier later on

---
+++ src/__tests__/test.ts
@@ -0,0 +1,39 @@
+import implementations from '../index';
+import { Game } from '../types/bowling.type';
+import { TestData } from './test.types';
+import { data } from './testGame.data';
+
+const result = { pass: 0, fail: 0 };
+const version = process.argv[2];
+
+function test([name, obj]: [string, TestData]): void {
+  const calculatedScore = implementations[version](obj.scores);
+  const status = calculatedScore === obj.expectedScore ? 'pass' : 'fail';
+  result[status]++;
+
+  console.log(
+    `${status === 'pass' ? '✅' : '❌'} ${name}: calculated score is ${calculatedScore} and expected score is ${
+      obj.expectedScore
+    } => ${status.toUpperCase()}`,
+  );
+}
+
+function main() {
+  if (!version || typeof implementations[version] !== 'function') {
+    console.error(`Invalid version number provided: ${version}`);
+    process.exit(1);
+  }
+  const specificGame = process.argv[3];
+
+  console.log(
+    `⏱️ Running ${version} ${specificGame ? `for ${specificGame}` : 'for all games'}`,
+  );
+
+  if (specificGame) {
+    test([specificGame, data[specificGame]]);
+  } else {
+    Object.entries(data).forEach(test);
+  }
+}
+
+main();
